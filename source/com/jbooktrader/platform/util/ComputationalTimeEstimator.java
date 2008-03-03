0cd7d80111c2973ab44aa31ebff1493d4383c83c
eugene.kononov <eugene.kononov@localhost>
2008-03-03 02:57:09 +0000

    refactored

--- source/com/jbooktrader/platform/util/ComputationalTimeEstimator.java
+++ source/com/jbooktrader/platform/util/ComputationalTimeEstimator.java
@@ -1,14 +1,21 @@
 package com.jbooktrader.platform.util;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 /**
  * "Remaining time" estimator for long-running computational processes.
  */
 public class ComputationalTimeEstimator {
+    private final static long MILLIS_IN_DAY = 1000 * 60 * 60 * 24;
     private final long startTime, totalIterations;
+    private final SimpleDateFormat sdf;
 
     public ComputationalTimeEstimator(long startTime, long totalIterations) {
         this.startTime = startTime;
         this.totalIterations = totalIterations;
+        sdf = new SimpleDateFormat("HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
     }
 
     public String getTimeLeft(long iterationsSoFar) {
@@ -19,17 +26,11 @@ public class ComputationalTimeEstimator {
             long millisPerIteration = elapsedTime / iterationsSoFar;
             long remainingMillis = millisPerIteration * (totalIterations - iterationsSoFar);
 
-            long remaining = remainingMillis / 1000;
-
-            remaining /= 60;
-            int remainingMins = (int) (remaining % 60);
-            int remainingHours = (int) remaining / 60;
-
-
-            if (remainingHours == 0 && remainingMins == 0) {
-                timeLeft = "Less than 1 minute";
+            long remainingDays = remainingMillis / MILLIS_IN_DAY;
+            if (remainingDays == 0) {
+                timeLeft = sdf.format(new Date(remainingMillis));
             } else {
-                timeLeft = remainingHours + " hour(s), " + remainingMins + " minute(s)";
+                timeLeft = "more than " + remainingDays + " days";
             }
         }
 
