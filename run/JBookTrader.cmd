23a143226edbb3137515c55321f1a629a31f958d
eugene.kononov <eugene.kononov@localhost>
2008-03-16 18:48:06 +0000

    added parallel GC options for better performance

--- run/JBookTrader.cmd
+++ run/JBookTrader.cmd
@@ -10,9 +10,8 @@ set cp=%cp%;%appHome%/lib/jcommon-1.0.9.jar
 set cp=%cp%;%appHome%/lib/jfreechart-1.0.6.jar
 
 set javaHome=C:/WINDOWS/system32/
-set javaOptions=-Xmx768M
+set javaOptions=-Xmx512M -XX:+UseParallelGC -XX:+AggressiveHeap
 set mainClass=com.jbooktrader.platform.startup.JBookTrader
-
 
 %javaHome%javaw.exe -cp "%cp%" %javaOptions% %mainClass% "%appHome%"
 
--- run/JBookTrader.sh
+++ run/JBookTrader.sh
@@ -28,7 +28,7 @@ mkdir -p bin
 find bin    -name "*.class" | xargs rm -f
 find source -name "*.java"  | xargs javac -cp "$CLASSPATH" -d bin
 
-JVM_OPTS="-Xmx1024m"
+JVM_OPTS="-Xmx1024m  -XX:+UseParallelGC -XX:+AggressiveHeap"
 [ -x "$(which uname)" ] && [ "$(uname -m)" == "x86_64" ] && JVM_OPTS="$JVM_OPTS -d64"
 
 exec java -cp "$CLASSPATH:$(pwd)/bin" $JVM_OPTS com.jbooktrader.platform.startup.JBookTrader "$(pwd)"
