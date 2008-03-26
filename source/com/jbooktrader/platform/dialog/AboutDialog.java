e33055670d9b3eca9e25360c2dbdf41f09a327e1
eugene.kononov <eugene.kononov@localhost>
2008-03-26 00:04:32 +0000

    ready for new release

--- source/com/jbooktrader/platform/dialog/AboutDialog.java
+++ source/com/jbooktrader/platform/dialog/AboutDialog.java
@@ -56,7 +56,7 @@ public class AboutDialog extends JDialog {
         aboutPanel.add(productValueLabel);
 
         JLabel versionLabel = new JLabel("Version:", JLabel.TRAILING);
-        JLabel versionValueLabel = new JLabel("2.03, March 23, 2008");
+        JLabel versionValueLabel = new JLabel("2.04, March 25, 2008");
         versionValueLabel.setForeground(Color.BLACK);
         versionLabel.setLabelFor(versionValueLabel);
         aboutPanel.add(versionLabel);
