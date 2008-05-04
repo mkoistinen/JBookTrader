320d01ee47904bf2bd04570be6323dfacf22cd21
eugene.kononov <eugene.kononov@localhost>
2008-05-04 03:50:46 +0000

    new indicator

---
+++ source/com/jbooktrader/indicator/RegressedDepthBalance.java
@@ -0,0 +1,37 @@
+package com.jbooktrader.indicator;
+
+import com.jbooktrader.platform.indicator.*;
+import com.jbooktrader.platform.marketdepth.*;
+
+/**
+ * Regressed depth balance
+ */
+public class RegressedDepthBalance extends Indicator {
+    private final int period;
+
+    public RegressedDepthBalance(MarketBook marketBook, int period) {
+        super(marketBook);
+        this.period = period;
+    }
+
+    @Override
+    public double calculate() {
+        int index = marketBook.size() - period - 1;
+
+        double sumX = 0, sumY = 0, sumXY = 0, sumXX = 0;
+        for (int x = 1; x <= period; x++) {
+            sumX += x;
+            int y = marketBook.getMarketDepth(++index).getBalance();
+            sumY += y;
+            sumXY += x * y;
+            sumXX += x * x;
+        }
+
+        double denom = period * sumXX - sumX * sumX;
+        double intercept = (sumXX * sumY - sumX * sumXY) / denom;
+        double slope = (period * sumXY - sumX * sumY) / denom;
+        value = intercept + slope * period;
+        return value;
+
+    }
+}
