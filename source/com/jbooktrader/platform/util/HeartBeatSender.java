a4f8af50f0904158233dc4fc28579de564858486
eugene.kononov <eugene.kononov@localhost>
2008-07-02 23:43:31 +0000

    Hearbeat is sent only when there is at least one active strategy.

--- source/com/jbooktrader/platform/util/HeartBeatSender.java
+++ source/com/jbooktrader/platform/util/HeartBeatSender.java
@@ -4,28 +4,58 @@ import com.jbooktrader.platform.model.*;
 import static com.jbooktrader.platform.model.Dispatcher.Mode.*;
 import static com.jbooktrader.platform.preferences.JBTPreferences.*;
 import com.jbooktrader.platform.preferences.*;
+import com.jbooktrader.platform.strategy.*;
 
+import java.util.*;
 import java.util.concurrent.*;
 
 public class HeartBeatSender {
+    private final List<Strategy> strategies;
+    private static HeartBeatSender instance;
 
     class Sender implements Runnable {
         public void run() {
-            Dispatcher.Mode mode = Dispatcher.getMode();
-            if (mode == Trade || mode == ForwardTest) {
-                String message = "Heart beat message: ";
-                boolean isConnected = Dispatcher.getTrader().getAssistant().isConnected();
-                if (isConnected) {
-                    message += "JBT operating normally.";
-                } else {
-                    message += "TWS was disconnected from IB server.";
+            long instant = System.currentTimeMillis();
+            boolean isTradingPeriod = false;
+            for (Strategy strategy : strategies) {
+                if (strategy.getTradingSchedule().contains(instant)) {
+                    isTradingPeriod = true;
+                    break;
                 }
-                SecureMailSender.getInstance().send(message);
+            }
+
+            if (isTradingPeriod) {
+                Dispatcher.Mode mode = Dispatcher.getMode();
+                if (mode == Trade || mode == ForwardTest) {
+                    String message = "Heart beat message: ";
+                    boolean isConnected = Dispatcher.getTrader().getAssistant().isConnected();
+                    if (isConnected) {
+                        message += "JBT operating normally.";
+                    } else {
+                        message += "TWS was disconnected from IB server.";
+                    }
+                    SecureMailSender.getInstance().send(message);
+                }
             }
         }
     }
 
-    public HeartBeatSender() {
+    public static HeartBeatSender getInstance() {
+        if (instance == null) {
+            instance = new HeartBeatSender();
+        }
+
+        return instance;
+    }
+
+
+    public void addStrategy(Strategy strategy) {
+        strategies.add(strategy);
+    }
+
+    // private constructor for non-instantiability
+    private HeartBeatSender() {
+        strategies = new ArrayList<Strategy>();
         PreferencesHolder prefs = PreferencesHolder.getInstance();
         if (prefs.get(EmailMonitoring).equalsIgnoreCase("enabled")) {
             int minutes = Integer.parseInt(prefs.get(HeartBeatInterval));
