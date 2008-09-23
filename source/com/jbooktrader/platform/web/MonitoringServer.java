e2f1ac69faa67c6e9432b9bca1aedada88f6b6f7
florent.guiliani <florent.guiliani@localhost>
2008-09-23 13:40:21 +0000

    Add and use conf/jetty.xml to configure the embeded http server.

--- source/com/jbooktrader/platform/web/MonitoringServer.java
+++ source/com/jbooktrader/platform/web/MonitoringServer.java
@@ -1,10 +1,14 @@
 package com.jbooktrader.platform.web;
 
+import java.io.FileInputStream;
+
 import com.jbooktrader.platform.model.Dispatcher;
 import com.jbooktrader.platform.model.*;
+import com.jbooktrader.platform.startup.JBookTrader;
 import com.jbooktrader.platform.util.*;
 import org.mortbay.jetty.*;
 import org.mortbay.jetty.servlet.*;
+import org.mortbay.xml.XmlConfiguration;
 
 public class MonitoringServer {
     private static boolean isStarted;
@@ -14,7 +18,9 @@ public class MonitoringServer {
             return;
 
         try {
-            Server server = new Server(1234);
+            Server server = new Server();
+            XmlConfiguration configuration = new XmlConfiguration(new FileInputStream(JBookTrader.getAppPath()+"/conf/jetty.xml"));
+            configuration.configure(server);
             Context context = new Context(server, "/", Context.SESSIONS);
             context.addServlet(new ServletHolder(new JBTServlet()), "/*");
             server.start();
