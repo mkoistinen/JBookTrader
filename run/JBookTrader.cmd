52771fee0f093d91f381463dbfd0feb11eebf2ef
eugene.kononov <eugene.kononov@localhost>
2008-09-23 03:24:29 +0000

    added jetty jars to startup Windows script

--- run/JBookTrader.cmd
+++ run/JBookTrader.cmd
@@ -10,8 +10,13 @@ set cp=%cp%;%appHome%/lib/mail.jar
 set cp=%cp%;%appHome%/lib/activation.jar
 set cp=%cp%;%appHome%/lib/jcommon-1.0.13.jar
 set cp=%cp%;%appHome%/lib/jfreechart-1.0.10.jar
+set cp=%cp%;%appHome%/lib/jetty-6.1.11.jar
+set cp=%cp%;%appHome%/lib/jetty-util-6.1.11.jar
+set cp=%cp%;%appHome%/lib/servlet-api-2.5-6.1.11.jar
+
 
 set javaOptions=-XX:+AggressiveHeap
 set mainClass=com.jbooktrader.platform.startup.JBookTrader
 
-%javaHome%javaw.exe -cp "%cp%" %javaOptions% %mainClass% "%appHome%"
+%javaHome%java.exe -cp "%cp%" %javaOptions% %mainClass% "%appHome%"
+
