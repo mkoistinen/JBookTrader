338856ecc484be5dfe863a402f23a100fe062903
florent.guiliani <florent.guiliani@localhost>
2008-09-24 16:48:48 +0000

    add PriceEMA indicator

---
+++ source/com/jbooktrader/indicator/price/PriceEMA.java
@@ -0,0 +1,20 @@
+package com.jbooktrader.indicator.price;
+
+import com.jbooktrader.platform.indicator.Indicator;
+
+public class PriceEMA extends Indicator {
+    private final double multiplier;
+
+    public PriceEMA(int length) {
+        multiplier = 2. / (length + 1.);        
+    }
+
+    @Override
+    public double calculate() {
+        double price = marketBook.getLastMarketSnapshot().getMidPrice();
+        value += (price - value) * multiplier;
+
+        return value;
+    }
+
+}
