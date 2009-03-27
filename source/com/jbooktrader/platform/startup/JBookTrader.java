98e1c45c389674d7660aa6a8c3980318a05bfcb9
eugene.kononov <eugene.kononov@localhost>
2009-03-27 03:29:22 +0000

    Updated for release 6.08.

--- source/com/jbooktrader/platform/startup/JBookTrader.java
+++ source/com/jbooktrader/platform/startup/JBookTrader.java
@@ -15,8 +15,8 @@ import java.nio.channels.*;
  */
 public class JBookTrader {
     public static final String APP_NAME = "JBookTrader";
-    public static final String VERSION = "6.07";
-    public static final String RELEASE_DATE = "March 21, 2009";
+    public static final String VERSION = "6.08";
+    public static final String RELEASE_DATE = "March 27, 2009";
     private static String appPath;
 
     /**
