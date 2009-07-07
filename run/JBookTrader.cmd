932304189349cacbeb299027814ed598e6e838b3
eugene.kononov <eugene.kononov@localhost>
2009-07-07 19:35:49 +0000

    Updated jcommons to version 1.0.16 and jfreechart to version 1.0.13

--- run/JBookTrader.cmd
+++ run/JBookTrader.cmd
@@ -4,13 +4,11 @@ set javaHome=C:/WINDOWS/system32/
 set cp=%appHome%
 set cp=%cp%;%appHome%/classes
 set cp=%cp%;%appHome%/resources
-set cp=%cp%;%appHome%/lib/activation.jar
 set cp=%cp%;%appHome%/lib/ibapi-9.62.jar
-set cp=%cp%;%appHome%/lib/jcommon-1.0.15.jar
-set cp=%cp%;%appHome%/lib/jfreechart-1.0.12.jar
+set cp=%cp%;%appHome%/lib/jcommon-1.0.16.jar
+set cp=%cp%;%appHome%/lib/jfreechart-1.0.13.jar
 set cp=%cp%;%appHome%/lib/junit-4.5.jar
 set cp=%cp%;%appHome%/lib/liquidlnf.jar
-set cp=%cp%;%appHome%/lib/mail.jar
 
 set javaOptions=-XX:+AggressiveHeap
 set mainClass=com.jbooktrader.platform.startup.JBookTrader
