ec8eb7b97e56441451c646b3f6ffc23531e7cae9
mkoistinen <mkoistinen@localhost>
2009-07-10 22:48:55 +0000

    Substantial update to look and feel for all platforms.

--- resources/web/iphone.js
+++ resources/web/iphone.js
@@ -1,10 +1,18 @@
-var updateLayout = function() {
-   if (window.innerWidth != currentWidth) {
-	  currentWidth = window.innerWidth;
-		 var orient = (currentWidth == 320) ? "profile" : "landscape";
-	  document.body.setAttribute("orient", orient);
-	  window.scrollTo(0, 1);
-   }
-};
-iPhone.DomLoad(updateLayout);
-setInterval(updateLayout, 500);
+addEventListener("load", function() { setTimeout(updateLayout, 0); });
+
+var currentWidth = 0;
+
+function updateLayout()
+{
+	if (window.innerWidth != currentWidth)
+	{
+		currentWidth = window.innerWidth;
+		var orient = currentWidth == 320 ? "profile" : "landscape";
+		document.body.setAttribute("orient", orient);
+		setTimeout(function() {
+			window.scrollTo(0, 1);
+		}, 100);
+	}
+}
+
+setInterval(updateLayout, 400);
