f3c90274427d3563e50f63332bc3a016ac0ad3df
eugene.kononov <eugene.kononov@localhost>
2009-07-11 05:27:16 +0000

    Removed unnecessary methods.

--- source/com/jbooktrader/platform/dialog/JBTDialog.java
+++ source/com/jbooktrader/platform/dialog/JBTDialog.java
@@ -1,98 +1,33 @@
 package com.jbooktrader.platform.dialog;
 
-import java.awt.Dialog;
-import java.awt.Frame;
-import java.awt.GraphicsConfiguration;
-import java.awt.Point;
-import java.awt.Window;
-import javax.swing.JDialog;
-import com.jbooktrader.platform.startup.JBookTrader;
-import com.jbooktrader.platform.util.MessageDialog;
+import com.jbooktrader.platform.startup.*;
+import com.jbooktrader.platform.util.*;
+
+import javax.swing.*;
+import java.awt.*;
 
 
 public class JBTDialog extends JDialog {
 
-	public JBTDialog() { }
+    public JBTDialog(Frame owner) {
+        super(owner);
+    }
 
-	public JBTDialog(Frame owner) {
-		super(owner);
-	}
+    public void setVisible(boolean visible) {
+        super.setVisible(visible);
 
-	public JBTDialog(Dialog owner) {
-		super(owner);
-	}
-
-	public JBTDialog(Window owner) {
-		super(owner);
-	}
-
-	public JBTDialog(Frame owner, boolean modal) {
-		super(owner, modal);
-	}
-
-	public JBTDialog(Frame owner, String title) {
-		super(owner, title);
-	}
-
-	public JBTDialog(Dialog owner, boolean modal) {
-		super(owner, modal);
-	}
-
-	public JBTDialog(Dialog owner, String title) {
-		super(owner, title);
-	}
-
-	public JBTDialog(Window owner, ModalityType modalityType) {
-		super(owner, modalityType);
-	}
-
-	public JBTDialog(Window owner, String title) {
-		super(owner, title);
-	}
-
-	public JBTDialog(Frame owner, String title, boolean modal) {
-		super(owner, title, modal);
-	}
-
-	public JBTDialog(Dialog owner, String title, boolean modal) {
-		super(owner, title, modal);
-	}
-
-	public JBTDialog(Window owner, String title, ModalityType modalityType) {
-		super(owner, title, modalityType);
-	}
-
-	public JBTDialog(Frame owner, String title, boolean modal,
-	    GraphicsConfiguration gc) {
-		super(owner, title, modal, gc);
-	}
-
-	public JBTDialog(Dialog owner, String title, boolean modal,
-	    GraphicsConfiguration gc) {
-		super(owner, title, modal, gc);
-	}
-
-	public JBTDialog(Window owner, String title, ModalityType modalityType,
-	    GraphicsConfiguration gc) {
-		super(owner, title, modalityType, gc);
-	}
-	
-	public void setVisible(boolean visible)
-	{
-		super.setVisible(visible);
-		
-		// This is to prevent the dialog from being drawn under the Mac menu bar.
+        // This is to prevent the dialog from being drawn under the Mac menu bar.
         try {
-	        if (JBookTrader.onMac()) {
-	        	Point loc = this.getLocation();
-	        	if (loc.getY() < JBookTrader.MAC_MENUBAR_HEIGHT)
-	        		loc.translate(0, (int) (JBookTrader.MAC_MENUBAR_HEIGHT-loc.getY()));
-	        	this.setLocation(loc);
-	        }
+            if (JBookTrader.onMac()) {
+                Point point = getLocation();
+                if (point.getY() < JBookTrader.MAC_MENUBAR_HEIGHT) {
+                    point.translate(0, (int) (JBookTrader.MAC_MENUBAR_HEIGHT - point.getY()));
+                }
+                setLocation(point);
+            }
         }
-        catch (Exception e)
-        {
-        	MessageDialog.showMessage(null, e.getMessage());
+        catch (Exception e) {
+            MessageDialog.showMessage(null, e.getMessage());
         }
-	}
+    }
 }
