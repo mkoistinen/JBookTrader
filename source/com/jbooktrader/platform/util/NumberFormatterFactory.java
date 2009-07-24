99fc20e487a4b7f8f73d981b9cba9e2976ff5691
mkoistinen <mkoistinen@localhost>
2009-07-24 21:59:08 +0000

    Refactored to remove redundancy.

--- source/com/jbooktrader/platform/util/NumberFormatterFactory.java
+++ source/com/jbooktrader/platform/util/NumberFormatterFactory.java
@@ -5,15 +5,7 @@ import java.text.*;
 public class NumberFormatterFactory {
 
     public static DecimalFormat getNumberFormatter(int maxFractionDigits) {
-        DecimalFormat decimalFormat = (DecimalFormat) NumberFormat.getNumberInstance();
-        DecimalFormatSymbols decimalFormatSeparator = new DecimalFormatSymbols();
-        decimalFormatSeparator.setDecimalSeparator('.');
-        decimalFormat.setGroupingUsed(false);
-        decimalFormat.setMinimumFractionDigits(0);
-        decimalFormat.setMaximumFractionDigits(maxFractionDigits);
-        decimalFormat.setDecimalFormatSymbols(decimalFormatSeparator);
-
-        return decimalFormat;
+    	return getNumberFormatter(maxFractionDigits, false);
     }
 
     public static DecimalFormat getNumberFormatter(int maxFractionDigits, boolean grouping) {
