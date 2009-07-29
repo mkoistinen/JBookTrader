ce9406744e4bcdbd45e537d1a26b06ef185ab5a0
eugene.kononov <eugene.kononov@localhost>
2009-07-29 21:02:20 +0000

    Updated.

--- source/com/jbooktrader/strategy/SecondNature.java
+++ source/com/jbooktrader/strategy/SecondNature.java
@@ -1,14 +1,20 @@
 package com.jbooktrader.strategy;
 
+import com.ib.client.*;
 import com.jbooktrader.indicator.velocity.*;
+import com.jbooktrader.platform.chart.*;
+import com.jbooktrader.platform.commission.*;
 import com.jbooktrader.platform.indicator.*;
 import com.jbooktrader.platform.model.*;
 import com.jbooktrader.platform.optimizer.*;
+import com.jbooktrader.platform.schedule.*;
+import com.jbooktrader.platform.strategy.*;
+import com.jbooktrader.platform.util.*;
 
 /**
  *
  */
-public class SecondNature extends StrategyES {
+public class SecondNature extends Strategy {
 
     // Technical indicators
     private final Indicator balanceVelocityInd, trendVelocityInd;
@@ -18,16 +24,24 @@ public class SecondNature extends StrategyES {
     private static final String SLOW_PERIOD = "Slow Period";
     private static final String TREND_PERIOD = "Trend Period";
     private static final String BALANCE_ENTRY = "Balance Entry";
-    private static final String TREND_ENTRY = "Trend Entry";
 
     // Strategy parameters values
-    private final int balanceEntry, trendEntry;
+    private final int balanceEntry;
 
     public SecondNature(StrategyParams optimizationParams) throws JBookTraderException {
         super(optimizationParams);
 
+        // Specify the contract to trade
+        Contract contract = ContractFactory.makeFutureContract("ES", "GLOBEX");
+        // Define trading schedule
+        TradingSchedule tradingSchedule = new TradingSchedule("10:00", "14:00", "America/New_York");
+        int multiplier = 50;// contract multiplier
+        double bidAskSpread = 0.25; // prevalent spread between best bid and best ask
+        Commission commission = CommissionFactory.getBundledNorthAmericaFutureCommission();
+        setStrategy(contract, tradingSchedule, multiplier, commission, bidAskSpread, BarSize.Minute1);
+
+
         balanceEntry = getParam(BALANCE_ENTRY);
-        trendEntry = getParam(TREND_ENTRY);
         balanceVelocityInd = new BalanceVelocity(getParam(FAST_PERIOD), getParam(SLOW_PERIOD));
         trendVelocityInd = new TrendStrengthVelocity(getParam(TREND_PERIOD));
         addIndicator(balanceVelocityInd);
@@ -42,11 +56,10 @@ public class SecondNature extends StrategyES {
      */
     @Override
     public void setParams() {
-        addParam(FAST_PERIOD, 120, 200, 1, 243);
-        addParam(SLOW_PERIOD, 4000, 9000, 500, 5291);
-        addParam(TREND_PERIOD, 100, 2000, 100, 1102);
-        addParam(BALANCE_ENTRY, 12, 20, 1, 14);
-        addParam(TREND_ENTRY, -2, 1, 1, -1);
+        addParam(FAST_PERIOD, 80, 230, 1, 105);
+        addParam(SLOW_PERIOD, 5500, 7500, 100, 6252);
+        addParam(TREND_PERIOD, 500, 1500, 100, 722);
+        addParam(BALANCE_ENTRY, 12, 21, 1, 19);
     }
 
     /**
@@ -57,7 +70,7 @@ public class SecondNature extends StrategyES {
     public void onBookChange() {
         double balanceVelocity = balanceVelocityInd.getValue();
         double trendVelocity = trendVelocityInd.getValue();
-        if (trendVelocity < trendEntry) {
+        if (trendVelocity < 0) {
             if (balanceVelocity >= balanceEntry) {
                 setPosition(1);
             } else if (balanceVelocity <= -balanceEntry) {
