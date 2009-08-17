eb3dec133e67bc3320fd694a8e3a14ab4d4d325e
mkoistinen <mkoistinen@localhost>
2009-08-17 15:29:18 +0000

    Update to the ES base strategy and the addition of YM.

---
+++ source/com/jbooktrader/strategy/base/StrategyYM.java
@@ -0,0 +1,39 @@
+package com.jbooktrader.strategy.base;
+
+import com.ib.client.*;
+import com.jbooktrader.platform.chart.*;
+import com.jbooktrader.platform.commission.*;
+import com.jbooktrader.platform.model.*;
+import com.jbooktrader.platform.optimizer.*;
+import com.jbooktrader.platform.schedule.*;
+import com.jbooktrader.platform.strategy.*;
+import com.jbooktrader.platform.util.*;
+
+/**
+ *
+ */
+public abstract class StrategyYM extends Strategy {
+
+	protected static final double BIDASKSPREAD = 1.00; // prevalent spread between best bid and best ask
+
+	/*
+	 * MARGIN REQUIREMENTS for YM: ECBOT as of 13-July-2009
+	 * 
+	 * Initial Intra-day: $3,250
+	 * Intra-day Maintenance: $2,600
+	 * Initial Overnight: $6,500
+	 * Overnight Maintenance: $5,200
+	 */
+	
+    protected StrategyYM(StrategyParams optimizationParams) throws JBookTraderException {
+        super(optimizationParams);
+        // Specify the contract to trade
+        Contract contract = ContractFactory.makeFutureContract("YM", "ECBOT");
+        // Define trading schedule
+        TradingSchedule tradingSchedule = new TradingSchedule("10:00", "15:55", "America/New_York");
+        int multiplier = 5;// contract multiplier
+        Commission commission = CommissionFactory.getBundledNorthAmericaFutureCommission();
+        setStrategy(contract, tradingSchedule, multiplier, commission, BIDASKSPREAD, BarSize.Minute1);
+    }
+
+}
