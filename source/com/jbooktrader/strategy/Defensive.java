45a35d5169bf93f79f62e508109d524a7312e8df
eugene.kononov <eugene.kononov@localhost>
2009-08-19 03:35:12 +0000

    Added another sample strategy.

---
+++ source/com/jbooktrader/strategy/Defensive.java
@@ -0,0 +1,75 @@
+package com.jbooktrader.strategy;
+
+import com.jbooktrader.indicator.velocity.*;
+import com.jbooktrader.platform.indicator.*;
+import com.jbooktrader.platform.model.*;
+import com.jbooktrader.platform.optimizer.*;
+import com.jbooktrader.strategy.base.*;
+
+/**
+ *
+ */
+public class Defensive extends StrategyES {
+
+    // Technical indicators
+    private final Indicator balanceVelocityInd, trendVelocityInd;
+
+    // Strategy parameters names
+    private static final String FAST_PERIOD = "Fast Period";
+    private static final String SLOW_PERIOD = "Slow Period";
+    private static final String TREND_PERIOD = "Trend Period";
+    private static final String ENTRY = "Entry";
+
+
+    // Strategy parameters values
+    private final int entry;
+
+    public Defensive(StrategyParams optimizationParams) throws JBookTraderException {
+        super(optimizationParams);
+
+        entry = getParam(ENTRY);
+        balanceVelocityInd = new BalanceVelocity(getParam(FAST_PERIOD), getParam(SLOW_PERIOD));
+        trendVelocityInd = new TrendStrengthVelocity(getParam(TREND_PERIOD));
+        addIndicator(balanceVelocityInd);
+        addIndicator(trendVelocityInd);
+    }
+
+    /**
+     * Adds parameters to strategy. Each parameter must have 5 values:
+     * name: identifier
+     * min, max, step: range for optimizer
+     * value: used in backtesting and trading
+     */
+    @Override
+    public void setParams() {
+        addParam(FAST_PERIOD, 6, 50, 1, 10);
+        addParam(SLOW_PERIOD, 100, 4500, 5, 3860);
+        addParam(TREND_PERIOD, 50, 600, 5, 277);
+        addParam(ENTRY, 100, 190, 1, 173);
+    }
+
+    /**
+     * This method is invoked by the framework when an order book changes and the technical
+     * indicators are recalculated. This is where the strategy itself should be defined.
+     */
+    @Override
+    public void onBookChange() {
+        double balanceVelocity = balanceVelocityInd.getValue() * 10;
+        double trendVelocity = trendVelocityInd.getValue();
+        if (trendVelocity < 0) {
+            if (balanceVelocity >= entry) {
+                setPosition(1);
+            } else if (balanceVelocity <= -entry) {
+                setPosition(-1);
+            }
+        } else {
+            int currentPosition = getPositionManager().getPosition();
+            if (currentPosition > 0 && balanceVelocity <= -entry) {
+                setPosition(0);
+            }
+            if (currentPosition < 0 && balanceVelocity >= entry) {
+                setPosition(0);
+            }
+        }
+    }
+}
