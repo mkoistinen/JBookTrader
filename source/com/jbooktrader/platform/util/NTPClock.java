a4f77c24b18419f07b57f7cd7a373515b36e5370
eugene.kononov <eugene.kononov@localhost>
2009-08-24 00:01:45 +0000

    Revised.

--- source/com/jbooktrader/platform/util/NTPClock.java
+++ source/com/jbooktrader/platform/util/NTPClock.java
@@ -1,11 +1,13 @@
 package com.jbooktrader.platform.util;
 
+import com.jbooktrader.platform.model.*;
 import org.apache.commons.net.ntp.*;
 
 import java.net.*;
+import java.util.concurrent.*;
 
 /*
--------- Server Pools--------
+-------- Server Pools --------
 Worldwide: 	 pool.ntp.org
 Asia: 	asia.pool.ntp.org
 Europe: 	europe.pool.ntp.org
@@ -14,31 +16,66 @@ Oceania: 	oceania.pool.ntp.org
 South America: 	south-america.pool.ntp.org
 */
 public class NTPClock {
+    private static final String HOST_NAME = "ntp2.usno.navy.mil";
     private final NTPUDPClient ntp;
     private final InetAddress host;
+    private long offset;
 
-    public NTPClock() {
-        ntp = new NTPUDPClient();
-        //ntp.setDefaultTimeout(500);
-        try {
-            host = InetAddress.getByName("north-america.pool.ntp.org");
-        } catch (UnknownHostException uhe) {
-            throw new RuntimeException(uhe);
+    private class NTPClockPoller implements Runnable {
+        public void run() {
+            getOffset();
         }
     }
 
-    public long getTime() {
-        TimeInfo timeInfo = null;
+    private void getAttributes() {
+        try {
+            TimeInfo timeInfo = ntp.getTime(host);
+            timeInfo.computeDetails();
+            NtpV3Packet message = timeInfo.getMessage();
 
-        while (timeInfo == null) {
-            try {
-                timeInfo = ntp.getTime(host);
-            } catch (Exception e) {
-                // ignore and retry
-            }
+            String msg = "NTP clock: [name:" + message.getReferenceIdString();
+            msg += ", stratum:" + message.getStratum();
+            msg += ", mode:" + message.getModeName();
+            msg += ", precision:" + message.getPrecision();
+            msg += ", type:" + message.getType();
+            msg += ", dispersion (ms):" + message.getRootDispersionInMillisDouble();
+            msg += "]";
+
+            Dispatcher.getReporter().report(msg);
+
+        } catch (Exception e) {
+            Dispatcher.getReporter().report("Problem while requesting time from server " + HOST_NAME + ": " + e.getMessage());
+        }
+    }
+
+
+    private void getOffset() {
+        try {
+            TimeInfo timeInfo = ntp.getTime(host);
+            timeInfo.computeDetails();
+            offset = timeInfo.getOffset();
+        } catch (Exception e) {
+            Dispatcher.getReporter().report("Problem while requesting time from server " + HOST_NAME + ": " + e.getMessage());
+        }
+    }
+
+    public NTPClock() {
+        ntp = new NTPUDPClient();
+        ntp.setDefaultTimeout(5000);
+        try {
+            host = InetAddress.getByName(HOST_NAME);
+        } catch (UnknownHostException uhe) {
+            Dispatcher.getReporter().report(uhe);
+            throw new RuntimeException(uhe);
         }
 
-        timeInfo.computeDetails();
-        return System.currentTimeMillis() + timeInfo.getOffset();
+        getAttributes();
+        getOffset();
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleWithFixedDelay(new NTPClockPoller(), 0, 1, TimeUnit.MINUTES);
+    }
+
+    public long getTime() {
+        return System.currentTimeMillis() + offset;
     }
 }
