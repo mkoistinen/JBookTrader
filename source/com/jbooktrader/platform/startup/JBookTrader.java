18974c974bcac0d5ac1d98fe74c5ad83230f28b5
eugene.kononov <eugene.kononov@localhost>
2009-08-24 22:51:15 +0000

    Updated version info.

--- source/com/jbooktrader/platform/startup/JBookTrader.java
+++ source/com/jbooktrader/platform/startup/JBookTrader.java
@@ -13,8 +13,8 @@ import java.nio.channels.*;
  */
 public class JBookTrader {
     public static final String APP_NAME = "JBookTrader";
-    public static final String VERSION = "7.03-dev";
-    public static final String RELEASE_DATE = "TBD";
+    public static final String VERSION = "7.03";
+    public static final String RELEASE_DATE = "August 24, 2009";
     private static String appPath;
 
     /**
