dc025f8f2868f7c0f688339c61c3004aad9a60e0
eugene.kononov <eugene.kononov@localhost>
2009-09-07 03:53:00 +0000

    Updated version label to 7.05-dev.

--- source/com/jbooktrader/platform/startup/JBookTrader.java
+++ source/com/jbooktrader/platform/startup/JBookTrader.java
@@ -13,7 +13,7 @@ import java.nio.channels.*;
  */
 public class JBookTrader {
     public static final String APP_NAME = "JBookTrader";
-    public static final String VERSION = "7.04-dev";
+    public static final String VERSION = "7.05-dev";
     public static final String RELEASE_DATE = "TBD";
     private static String appPath;
 
