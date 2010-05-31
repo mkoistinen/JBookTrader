15b681335bb93733d1448576dc8b562b0612f460
eugene.kononov <eugene.kononov@localhost>
2010-05-31 18:12:52 +0000

    Upgraded to latest IB API and updated start up script.

--- run/JBookTrader.cmd
+++ run/JBookTrader.cmd
@@ -1,10 +1,11 @@
+echo off
 set appHome=..
 set javaHome=C:/WINDOWS/system32/
 
 set cp=%appHome%
 set cp=%cp%;%appHome%/classes
 set cp=%cp%;%appHome%/resources
-set cp=%cp%;%appHome%/lib/ibapi-9.62.jar
+set cp=%cp%;%appHome%/lib/ibapi-9.63.jar
 set cp=%cp%;%appHome%/lib/jcommon-1.0.16.jar
 set cp=%cp%;%appHome%/lib/jfreechart-1.0.13.jar
 set cp=%cp%;%appHome%/lib/junit-4.5.jar
@@ -16,5 +17,6 @@ set cp=%cp%;%appHome%/lib/commons-net-2.0.jar
 set javaOptions=-XX:+AggressiveHeap
 set mainClass=com.jbooktrader.platform.startup.JBookTrader
 
+echo on
 %javaHome%java.exe -cp "%cp%" %javaOptions% %mainClass% "%appHome%"
 
