de74c914b14b829c1af70f4d1f414097f850bfc8
eugene.kononov <eugene.kononov@localhost>
2013-01-10 20:26:16 -0500

    Code formatting.

--- source/com/jbooktrader/platform/model/MainFrameController.java
+++ source/com/jbooktrader/platform/model/MainFrameController.java
@@ -4,7 +4,6 @@ import com.jbooktrader.platform.backtest.*;
 import com.jbooktrader.platform.chart.*;
 import com.jbooktrader.platform.dialog.*;
 import com.jbooktrader.platform.optimizer.*;
-import static com.jbooktrader.platform.preferences.JBTPreferences.*;
 import com.jbooktrader.platform.preferences.*;
 import com.jbooktrader.platform.startup.*;
 import com.jbooktrader.platform.strategy.*;
@@ -14,6 +13,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.net.*;
+
+import static com.jbooktrader.platform.preferences.JBTPreferences.*;
 
 /**
  * Acts as a controller in the Model-View-Controller pattern
