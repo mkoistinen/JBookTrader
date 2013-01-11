379a1b251e219633182842dcdcec3c81f0ac8852
eugene.kononov <eugene.kononov@localhost>
2013-01-10 20:25:50 -0500

    Utility class for efficient calculation of simple moving averages.

---
+++ source/com/jbooktrader/platform/util/movingwindow/MovingWindowSum.java
@@ -0,0 +1,27 @@
+package com.jbooktrader.platform.util.movingwindow;
+
+/**
+ */
+public class MovingWindowSum extends MovingWindow {
+    private double sum;
+
+    public MovingWindowSum(int size) {
+        super(size);
+    }
+
+    @Override
+    public void add(double value) {
+        sum = sum + value - getFirst();
+        super.add(value);
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    @Override
+    public void clear() {
+        sum = 0;
+        super.clear();
+    }
+}
